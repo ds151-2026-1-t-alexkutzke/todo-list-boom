e7388aac13c524e80c160b548692a6e8eae25378
pauloschiochetufpr <paulsix02@gmail.com>
2026-03-18 17:47:35 -0300

    todo list funcional

--- App.tsx
+++ App.tsx
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import TodoList from './src/todolista';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </SafeAreaView >
-    </SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
+      <TodoList />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: StatusBar.currentHeight ?? 0,
   },
 });
